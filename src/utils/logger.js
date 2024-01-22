d682371dd66cc0348cba37d0a18a814e2f4b70c0
caiqian <zero530@126.com>
2024-01-22 13:54:24 +0800

    add logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,15 @@
+// logger.js
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        }),
+        new winston.transports.File({filename: 'app.log'}),
+    ],
+});
+
+module.exports = logger;
