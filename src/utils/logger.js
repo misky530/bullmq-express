edd4135f744ecf0fe85266c59fd95aa085ba331d
caiqian <zero530@126.com>
2024-01-22 14:10:09 +0800

    add logger.js with rotate

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,15 +1,34 @@
-// logger.js
+require('winston-daily-rotate-file');
 const winston = require('winston');
 
+// const logger = winston.createLogger({
+//     level: 'info',
+//     format: winston.format.json(),
+//     transports: [
+//         new winston.transports.Console({
+//             format: winston.format.simple(),
+//         }),
+//         new winston.transports.File({filename: 'app.log'}),
+//     ],
+// });
+
+
+const transport = new winston.transports.DailyRotateFile({
+    filename: 'mtim-task-%DATE%.log',
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '7d'
+});
+
+transport.on('rotate', function (oldFilename, newFilename) {
+    // do something fun
+});
+
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
     transports: [
-        new winston.transports.Console({
-            format: winston.format.simple(),
-        }),
-        new winston.transports.File({filename: 'app.log'}),
-    ],
+        transport
+    ]
 });
 
 module.exports = logger;
