e344843d564fc665cf4b67c76e4c04d037be9a13
caiqian <zero530@126.com>
2024-03-07 11:17:45 +0800

    add dha method

---
+++ src/td/dha.ts
@@ -0,0 +1,19 @@
+export class Dha {
+    public static async getPropsHistoryData(propIds: string, timeStart: string, timeEnd: string): Promise<any> {
+        return {}
+    }
+
+    public static async getPropsHistoryValueByTime(propIds: string, time: string, mode: number): Promise<any> {
+        return {}
+    }
+
+    public static async getPropsHistoryValueByTimeList(propIds: string, mode: number, timeList: string): Promise<any> {
+        return {}
+    }
+
+    public static async getPropsHistoryValue(propIds: string, timeStart: string, timeEnd: string, pcs: number
+        , interval: number, iUnit: string, mode: number, minMax: string
+        , extraVals: string, sort: string): Promise<any> {
+
+    }
+}
